e3688c19ec88769dbd8817f0238d7b36c941ab0a
Owain Llewellyn <owain.llew@gmail.com>
2013-07-25 13:41:04 +0100

    added sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,23 @@
  
 module.exports = function (grunt) {
  
-  grunt.loadNpmTasks();
+  grunt.loadNpmTasks('grunt-contrib-sass');
  
   // set up config
   grunt.initConfig({
+  	sass: {                              // Task
+	    dist: {                            // Target
+	      options: {                       // Target options
+	        style: 'expanded'
+	      },
+	      files: {                         // Dictionary of files
+	        'css/main.css': 'sass/main.sass'
+	      }
+	    }
+	  }
  
   });
  
   // register a few tasks
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['sass']);
 };
