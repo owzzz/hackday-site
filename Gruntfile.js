629a4ca5b027f15927564fc0c0ac8d795d5cf348
Owain Llewellyn <owain.llew@gmail.com>
2013-07-25 13:23:31 +0100

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+module.exports = function (grunt) {
+ 
+  grunt.loadNpmTasks();
+ 
+  // set up config
+  grunt.initConfig({
+ 
+  });
+ 
+  // register a few tasks
+  grunt.registerTask('default', []);
+};
